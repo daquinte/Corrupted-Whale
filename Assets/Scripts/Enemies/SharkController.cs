b32aeac0ba22eed6b1bd89858ffb2ed99e45be37
adralcan <adrian97ad@gmail.com>
2018-05-05 14:20:07 +0200

    NavMesh basic hecha

---
+++ Assets/Scripts/Enemies/SharkController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SharkController : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent shark;
+	// Update is called once per frame
+	void Update () {
+
+        //transform.position = new Vector3(transform.position.z, - 1.5f, transform.position.z);
+		if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                // Mover el agente
+                shark.SetDestination(hit.point);
+            }
+        }
+	}
+}
