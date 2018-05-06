ddf3f7f19cd7b0d3d0c8b3adcd988db44ed86051
DPerezLuque <daniep04@ucm.es>
2018-05-06 02:46:13 +0200

    Escenas finales

---
+++ Assets/Scripts/Menus/WinMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("WaterSampleScene");
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Adiosito :3");
+        Application.Quit();
+    }
+}
